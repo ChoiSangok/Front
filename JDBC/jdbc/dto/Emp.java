253a3beff7818d1a0065260714ecfe33c1641771
ChoiSangok <60969294+ChoiSangok@users.noreply.github.com>
2020-05-18 19:14:35 +0900

    private toString

---
+++ JDBC/jdbc/dto/Emp.java
@@ -0,0 +1,74 @@
+package dto;
+
+import java.util.Date;
+
+public class Emp {
+	
+	//대문자 -> 소문자 단축기: ctrl + shift + y
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private Date hiredate; //java.util.Date
+	private double sal; //NUMBER(7,2)
+	private double comm; //NUMBER(7,2)
+	private int deptno;
+
+	
+
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", job=" + job + ", mgr=" + mgr + ", hiredate=" + hiredate
+				+ ", sal=" + sal + ", comm=" + comm + ", deptno=" + deptno + "]";
+	}
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	public double getComm() {
+		return comm;
+	}
+	public void setComm(double comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+}
